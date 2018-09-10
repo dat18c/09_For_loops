11cceb329622d98186a83e4fe2ea2fcb1247f713
JarlTuxen <jart@kea.dk>
2018-09-10 11:26:46 +0200

    for med metode

--- kode/ForLoop.java
+++ kode/ForLoop.java
@@ -3,7 +3,30 @@ public class ForLoop
 
    public static void main(String[] args)
    {
-      System.out.println("*");
+      for(int i=0;i<10;i++)
+      {
+         System.out.print("*");      
+      }
+      
+      //blank linie
+      System.out.println();
+      
+      for(int j=0;j<10;j++)
+      {
+         System.out.println(j);
+      }
+      //j out of scope
+      //System.out.println(j);
+      
+      //kald metode med parameteren 3
+      minPrint(3);
+      
+   }
+
+   //metode der kan kaldes fra main
+   public static void minPrint(int mitParameter)
+   {
+      
    }
 
 }
